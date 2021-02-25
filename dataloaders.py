ba08be96fbe30ee004f25fd84c4b0ba1fb61d233
alvin <alvin_heng95@hotmail.com>
2021-02-25 15:19:51 +0800

    train and test data are stored as .pt tensors

--- dataloaders.py
+++ dataloaders.py
@@ -2,22 +2,22 @@
 This file contains class definitions of Datasets that you can use with Pytorch's Dataloader.
 """
 
-from read_data import get_mias_data, get_dx_data
 import numpy as np
 from torch.utils.data import Dataset
+import torch
 
 class mias_dataset(Dataset):
     """
     Dataset class for MIAS (mammogram) data
     """
-    def __init__(self, dir, clean_transform, noise_transform):
+    def __init__(self, clean_mias_data, clean_transform, noise_transform):
         """
         Args:
             clean_transform: composition of transforms for the clean data
             noise_transform: composition of transforms for the noisy data
         """
-        self.clean_data = get_mias_data(dir)
-        self.noise_data = np.copy(self.clean_data)
+        self.clean_data = torch.clone(clean_mias_data)
+        self.noise_data = torch.clone(self.clean_data)
         self.clean_transform = clean_transform
         self.noise_transform = noise_transform
         
@@ -25,20 +25,20 @@ class mias_dataset(Dataset):
         return len(self.clean_data)
     
     def __getitem__(self, index):
-        return clean_transform(self.clean_data[index]), noise_transform(self.noise_data[index])
+        return self.clean_transform(self.clean_data[index]), self.noise_transform(self.noise_data[index])
     
 class dx_dataset(Dataset):
     """
     Dataset class for DX (dental) data
     """
-    def __init__(self, dir, clean_transform, noise_transform):
+    def __init__(self, clean_dx_data, clean_transform, noise_transform):
         """
         Args:
             clean_transform: composition of transforms for the clean data
             noise_transform: composition of transforms for the noisy data
         """
-        self.clean_data = get_dx_data(dir)
-        self.noise_data = np.copy(self.clean_data)
+        self.clean_data = torch.clone(clean_dx_data)
+        self.noise_data = torch.clone(self.clean_data)
         self.clean_transform = clean_transform
         self.noise_transform = noise_transform
         
@@ -46,5 +46,5 @@ class dx_dataset(Dataset):
         return len(self.clean_data)
     
     def __getitem__(self, index):
-        return clean_transform(self.clean_data[index]), noise_transform(self.noise_data[index])
+        return self.clean_transform(self.clean_data[index]), self.noise_transform(self.noise_data[index])
 
